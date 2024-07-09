d68f302b6cac4b8b48783515b49f5a1c6c410ce5
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-07-09 23:51:45 +0530

    Modification for static files

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import speedtest
 
 app = FastAPI()
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 @app.get("/speedtest")
 def run_speedtest():
