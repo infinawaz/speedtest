89b2442d67f2b62f9e7c6e1921d57cdae92383d2
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-07-10 01:49:08 +0530

    removed static

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import speedtest
+
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
-@app.get("/speedtest")
-def run_speedtest():
-    st = speedtest.Speedtest()
-    st.get_best_server()
-    download_speed = st.download()
-    upload_speed = st.upload()
-    ping = st.results.ping
-    return {
-        "download_speed": download_speed,
-        "upload_speed": upload_speed,
-        "ping": ping
-    }
+@app.get("/")
+def read_root():
+    with open("index.html") as f:
+        return HTMLResponse(f.read())
+
+
+@app.get("/api/x")
+def read():
+    return {"Hello": "X"}
+
+@app.get("/api/x/z")
+def read():
+    return {"Hello": "Z"}   
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, port=8000)
