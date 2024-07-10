74a766185ce77fe396c7db165b33545978d995c6
Md Shahnawaz Alam Khan <shahnawazdgp07@gmail.com>
2024-07-10 12:57:56 +0530

    endpoint /speedtest updated

--- main.py
+++ main.py
@@ -10,6 +10,19 @@ app = FastAPI()
 def read_root():
     with open("index.html") as f:
         return HTMLResponse(f.read())
+    
+@app.get("/speedtest")
+def run_speedtest():
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    download_speed = st.download()
+    upload_speed = st.upload()
+    ping = st.results.ping
+    return {
+        "download_speed": download_speed,
+        "upload_speed": upload_speed,
+        "ping": ping
+    }
 
 
 @app.get("/api/x")
